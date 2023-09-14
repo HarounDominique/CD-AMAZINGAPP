bb8f77259f482ebc3c70f0e85d7f6298ecaf1e3c
HAROUN <haroundominique@gmail.com>
2023-09-14 13:12:09 +0200

    productController done

---
+++ src/main/java/com/campusdual/appamazing/controller/ContactController.java
@@ -0,0 +1,4 @@
+package com.campusdual.appamazing.controller;
+
+public class ContactController {
+}
--- src/main/java/com/campusdual/appamazing/controller/ProductController.java
+++ src/main/java/com/campusdual/appamazing/controller/ProductController.java
@@ -1,13 +1,53 @@
 package com.campusdual.appamazing.controller;
 
+import com.campusdual.appamazing.api.IProductsService;
+import com.campusdual.appamazing.model.dto.ProductDto;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController()
 @RequestMapping("/products")
 public class ProductController {
-    
+
+    @Autowired
+    private IProductsService productsService;
+
     @PostMapping
     public String testController(@RequestBody String name){
         return "Products controller works, "+name+"!" ;
+    }
+
+    @PostMapping("/get")
+    public ProductDto queryProduct(@RequestBody ProductDto productDto){
+        return this.productsService.queryProduct(productDto);
+    }
+
+    @GetMapping("/get/{id}")
+    public ProductDto queryProduct(@PathVariable int id){
+        ProductDto productDto = new ProductDto();
+        productDto.setId(id);
+        return this.productsService.queryProduct(productDto);
+    }
+
+    @GetMapping("/getall")
+    public List<ProductDto> queryAllProducts(){
+        return this.productsService.queryAllProducts();
+    }
+
+    @PostMapping("/add")
+    public int addProduct(@RequestBody ProductDto productDto){
+        return this.productsService.insertProduct(productDto);
+    }
+
+    @PutMapping("/update")
+    public int updateProduct(@RequestBody ProductDto productDto){
+        return this.productsService.updateProduct(productDto);
+    }
+
+    @DeleteMapping("/delete")
+    public int deleteProduct(@RequestBody ProductDto productDto){
+        return this.productsService.deleteProduct(productDto);
     }
 }
