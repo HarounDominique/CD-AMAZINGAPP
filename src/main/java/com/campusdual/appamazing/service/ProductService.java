1b3eae0c50942a9926270116da8ea96dd0110780
HAROUN <haroundominique@gmail.com>
2023-09-14 09:52:28 +0200

    productService updating

--- src/main/java/com/campusdual/appamazing/service/ProductService.java
+++ src/main/java/com/campusdual/appamazing/service/ProductService.java
@@ -1,8 +1,10 @@
 package com.campusdual.appamazing.service;
 
 import com.campusdual.appamazing.api.IProductsService;
+import com.campusdual.appamazing.model.Product;
 import com.campusdual.appamazing.model.dao.ProductDao;
 import com.campusdual.appamazing.model.dto.ProductDto;
+import com.campusdual.appamazing.model.dto.dtomapper.ProductMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Service;
@@ -15,19 +17,27 @@ public class ProductService implements IProductsService {
     @Autowired
     private ProductDao productDao;
 
+//    @Autowired
+//    public ProductService (ProductDao productDao){
+//        this.productDao = productDao;
+//    }
+
     @Override
     public ProductDto queryProduct(ProductDto productDto) {
-        return null;
+        Product product = ProductMapper.INSTANCE.toEntity(productDto);
+        return ProductMapper.INSTANCE.toDTO(productDao.getReferenceById(product.getId()));
     }
 
     @Override
     public List<ProductDto> queryAllProducts() {
-        return null;
+        return ProductMapper.INSTANCE.toDTOList(productDao.findAll());
     }
 
     @Override
     public int insertProduct(ProductDto productDto) {
-        return 0;
+        Product product = ProductMapper.INSTANCE.toEntity(productDto);
+        productDao.saveAndFlush(product);
+        return product.getId();
     }
 
     @Override
